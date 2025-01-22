4a12be1dd9de1e31f7b67332ba6a19e93de81657
vian <swadikapkapunkap@gmail.com>
2025-01-23 01:53:07 +0800

    Perbarui kode

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,126 @@
+// random kata muda < 21
+const randomAgeYoung = [
+  "Muda tapi sudah punya banyak alasan untuk tidak melakukan apa-apa.",
+  "Kamu masih belajar, tapi sudah mahir dalam membuat kesalahan.",
+  "Umurmu masih muda, tapi sudah memiliki wawasan yang terlihat seperti sudah mengalami banyak hal dalam hidup.",
+  "Kamu masih di usia yang baik, tapi sudah memiliki kebiasaan yang buruk seperti orang tua.",
+  "Muda, tapi sudah kehabisan ide.",
+  "Kamu masih muda, tapi sudah memiliki rambut yang membatu.",
+  "Tua dalam kesalahan, muda dalam pengalaman.",
+  "Kamu masih muda, tapi sudah memiliki kuping yang tidak mendengar.",
+  "Pengalamanmu masih sedikit, tapi egoismu sudah besar.",
+  "Muda, tapi sudah terlalu percaya diri.",
+  "Kamu masih muda, tapi sudah memiliki mulut yang tajam.",
+  "Belum dewasa, tapi sudah berpikir dewasa.",
+  "Muda, tapi sudah memiliki hati yang keras.",
+  "Kamu masih muda, tapi sudah memiliki pandangan yang sempit.",
+  "Tua dalam pikiran, muda dalam tindakan.",
+  "Muda, tapi sudah kehilangan arah.",
+  "Kamu masih muda, tapi sudah memiliki kebiasaan buruk.",
+  "Muda, tapi sudah terlalu ambisius.",
+  "Kamu masih muda, tapi sudah memiliki masalah dewasa.",
+  "Muda, tapi sudah kehabisan impian.",
+  "Kamu masih muda, tapi sudah berpikir tua.",
+];
+
+//  Untuk Dewasa
+const randomAgeAdult = [
+  "Pengalamanmu sudah banyak, tapi masih belum bisa menghindari kesalahan muda.",
+  "Umurmu sudah dewasa, tapi masih memiliki semangat muda yang salah arah.",
+  "Kamu sudah dewasa, tapi masih belum bisa mengendalikan emosi.",
+  "Pengalaman hidupmu sudah panjang, tapi masih belum bisa membuat keputusan tepat.",
+  "Kamu sudah tua, tapi masih memiliki pikiran muda dan salah.",
+  "Tua dalam umur, muda dalam semangat.",
+  "Kamu sudah dewasa, tapi masih belum bisa melepaskan kebiasaan buruk.",
+  "Pengalamanmu sudah luas, tapi masih belum bisa mengerti orang lain.",
+  "Dewasa, tapi masih egois.",
+  "Kamu sudah tua, tapi masih memiliki hati muda.",
+  "Pengalamanmu sudah banyak, tapi masih belum bisa mengajarkan kesabaran.",
+  "Umurmu sudah dewasa, tapi masih memiliki pikiran anak-anak.",
+  "Kamu sudah dewasa, tapi masih belum bisa mengendalikan kata-kata.",
+  "Pengalaman hidupmu sudah panjang, tapi masih belum bisa membuatmu bijak.",
+  "Kamu sudah tua, tapi masih memiliki semangat muda.",
+  "Dewasa, tapi masih belum dewasa.",
+  "Kamu sudah dewasa, tapi masih belum bisa melepaskan masa lalu.",
+  "Pengalamanmu sudah luas, tapi masih belum bisa mengerti diri sendiri.",
+  "Umurmu sudah dewasa, tapi masih memiliki kebiasaan buruk.",
+  "Kamu sudah tua, tapi masih memiliki impian muda.",
+  "Kamu sudah dewasa, tapi masih belum bisa menghargai waktu.",
+];
+
+const randomMessages = [
+  "Harusnya belajar dulu, baru main-main.",
+  "Udah coba pake otak belom?",
+  "Ini calculator apa, petak umpet?",
+  "Coba cek lagi, jangan asal input.",
+  "Waduh, ini jawabannya dari mana?",
+  "Kayaknya angka kamu hilang deh.",
+  "Ini angka atau huruf? Bingung deh.",
+  "Aduh, ini input atau prank?",
+  "Baca dulu, baru coba lagi.",
+  "Kalau bisa, kasih yang bener dong.",
+  "Yakin ini angka? Bisa jadi kucing juga.",
+  "Masa gitu aja nggak ngerti?",
+  "Ini serius apa bercanda sih?",
+  "Inputnya ngaco deh, benerin dulu.",
+  "Bukan tugasnya Google, kamu yang salah.",
+  "Boleh nggak sih, sekali aja input yang bener?",
+  "Ini angka apa dongeng sih?",
+  "Kenapa nggak coba pake angka aja?",
+  "Coba fokus sedikit, biar nggak kayak gini.",
+  "Anda mencoba memberi saya alfabet?",
+  "Masukkan angka, bukan puisi.",
+  "Tolong, angka. Ini bukan soal esai.",
+  "Saya butuh angka, bukan ramalan bintang.",
+  "Ini bukan teka-teki silang, masukkan angka.",
+  "Ini bukan permainan scrabble, angka saja.",
+  "Mungkin kali ini coba masukkan angka.",
+  "Tolong, angka. Saya bukan mesin pembaca puisi.",
+  "Ingat, ini angka, bukan cerita pendek.",
+  "Ini bukan game tebak kata, masukkan angka",
+];
+
+//ambil umur dari input
+function getAgeInput() {
+  return document.getElementById("umur").value;
+}
+
+function calculateAge() {
+  const inputAge = getAgeInput();
+  const yearNow = new Date().getFullYear();
+  const result = yearNow - inputAge;
+  return result;
+}
+function resultAge() {
+  const age = calculateAge();
+  const randomAgeYoungArray =
+    randomAgeYoung[Math.floor(Math.random() * randomAgeYoung.length)];
+
+  const randomAgeAdultArray =
+    randomAgeAdult[Math.floor(Math.random() * randomAgeAdult.length)];
+
+  const randomMessagesArray =
+    randomMessages[Math.floor(Math.random() * randomMessages.length)];
+
+  const getResult = document.getElementById("hasil");
+  if (isNaN(age)) {
+    getResult.textContent = ` ${randomMessagesArray}`;
+  } else if (age < 21) {
+    getResult.textContent = `Umur :  ${age}, ${randomAgeYoungArray} `;
+  } else {
+    getResult.textContent = `Umur :  ${age}, ${randomAgeAdultArray} `;
+  }
+}
+
+function deletResults() {
+  const results = document.getElementById("hasil");
+  const ageInput = document.getElementById("umur");
+
+  results.textContent = ""; // Menghapus semua teks dalam elemen dengan id "hasil"
+  ageInput.value = ""; // Mengosongkan nilai dari input dengan id "umur"
+}
+//random kata tua > 21
+
 // gif random
 document.addEventListener("DOMContentLoaded", () => {
   const gifs = [
@@ -30,9 +153,9 @@ document.addEventListener("DOMContentLoaded", () => {
     "https://media.giphy.com/media/tXNMqUmtvyuIw/giphy.gif?cid=ecf05e47shrxk8nzem0govbywegi2yw95i8osegcfzbymswl&ep=v1_gifs_search&rid=giphy.gif&ct=g",
   ];
 
-  const random = Math.floor(Math.random() * gifs.length);
-  const randomGif = gifs[random];
-  document.querySelector("#gif-img").src = randomGif;
+  const random = gifs[Math.floor(Math.random() * gifs.length)];
+  // const randomGif = gifs[random];
+  document.querySelector("#gif-img").src = random;
 });
 
 //nav fixed
@@ -91,39 +214,87 @@ window.addEventListener("click", (e) => {
 //     : html.classList.remove("dark-gray");
 // });
 
+// document.addEventListener("DOMContentLoaded", function () {
+//   const darkModeToggle = document.querySelector("#dark-toggle");
+//   const html = document.querySelector("html");
+//   const darkModeSistem = window.matchMedia("(prefers-color-scheme: dark)");
+//   // Periksa preferensi pengguna di localStorage
+//   const savedDarkMode = localStorage.getItem("darkMode");
+
+//   //periksa mode gelap sistem
+
+//   //Pantau perubahan mode sistem
+
+//   //Periksa preferensi mode gelap yang tersimpan
+//   if (savedDarkMode === "enabled") {
+//     html.classList.add("dark");
+//     darkModeToggle.checked = true;
+//   } else if (savedDarkMode === "disabled") {
+//     html.classList.remove("dark");
+//     darkModeToggle.checked = false;
+//   } else if (darkModeSistem.matches) {
+//     html.classList.add("dark");
+//     darkModeToggle.checked = true;
+//   }
+
+//   darkModeSistem.addEventListener("change", () => {
+//     if (!savedDarkMode) {
+//       if (darkModeSistem.matches) {
+//         html.classList.add("dark");
+//       } else {
+//         html.classList.remove("dark");
+//       }
+//     }
+//   });
+
+//   darkModeToggle.addEventListener("change", () => {
+//     if (darkModeToggle.checked) {
+//       html.classList.add("dark");
+//       localStorage.setItem("darkMode", "enabled");
+//     } else {
+//       html.classList.remove("dark");
+//       localStorage.setItem("darkMode", "disabled");
+//     }
+//   });
+// });
+
 document.addEventListener("DOMContentLoaded", function () {
   const darkModeToggle = document.querySelector("#dark-toggle");
   const html = document.querySelector("html");
   const darkModeSistem = window.matchMedia("(prefers-color-scheme: dark)");
 
-  // periksa mode gelap sistem
-  if (darkModeSistem.matches) {
+  // Periksa preferensi pengguna di localStorage
+  const savedDarkMode = localStorage.getItem("darkMode");
+  // Kode ini: Fokus pada pengguna lama,
+  // memprioritaskan pilihan mereka sebelumnya.
+  if (savedDarkMode === "enabled") {
     html.classList.add("dark");
     darkModeToggle.checked = true;
-    localStorage.setItem("darkMode", "enabled");
-  }
-
-  // Periksa preferensi mode gelap yang tersimpan
-  if (localStorage.getItem("darkMode") === "enabled") {
-    darkModeToggle.checked = true;
+  } else if (savedDarkMode === "disabled") {
+    html.classList.remove("dark");
+    darkModeToggle.checked = false;
+  } else if (darkModeSistem.matches) {
+    // Jika tidak ada preferensi tersimpan, gunakan mode sistem
     html.classList.add("dark");
+    darkModeToggle.checked = true;
   }
 
-  darkModeSistema.addEventListener("change", () => {
-    if (darkModeSistem.matches) {
-      html.classList.add("dark");
-      darkModeToggle.checked = true;
-      localStorage.setItem("darkMode", "enabled");
-    } else {
-      html.classList.remove("dark");
-      darkModeToggle.checked = false;
-      localStorage.setItem("darkMode", "disabled");
+  //Kode ini: Fokus pada pengguna baru,
+  //  mengikuti mode sistem sebagai default sampai ada preferensi tersimpan.
+  darkModeSistem.addEventListener("change", () => {
+    if (!savedDarkMode) {
+      if (darkModeSistem.matches) {
+        html.classList.add("dark");
+      } else {
+        html.classList.remove("dark");
+      }
     }
   });
+
+  // Ubah mode sesuai pilihan pengguna
   darkModeToggle.addEventListener("change", () => {
     if (darkModeToggle.checked) {
       html.classList.add("dark");
-
       localStorage.setItem("darkMode", "enabled");
     } else {
       html.classList.remove("dark");
@@ -131,3 +302,5 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+//Ubah mode sesuai pilihan pengguna
